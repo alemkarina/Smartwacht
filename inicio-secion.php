80e34451cde4a77fe20dd268cc1188c396d15f3e
Franco <francoarganiaraz@gmail.com>
2020-01-16 19:51:01 -0300

    inicio de secion con validaciones y persistencia

---
+++ inicio-secion.php
@@ -0,0 +1,509 @@
+﻿<?php
+
+require_once 'controladores/funciones.php';
+$arrayDeErrores = "";
+//retorna un array de errores
+if ($_POST) {
+	$arrayDeErrores = validarRegistracion($_POST);
+	if ($arrayDeErrores) {
+		// Logueo al usuario
+		//obtengo el contenido del json
+		$arrayDeUsuarios = abrirBBDD("usuarios.json");
+		foreach ($arrayDeUsuarios as $usuarioJson) {
+			$userFinal = json_decode($usuarioJson, true);
+			if ($_POST["email"] == $userFinal["email"]) {
+				if (password_verify($_POST["pass"], $userFinal["pass"])) {
+					header("Location: index.html");
+					exit;
+				}
+			}
+		}
+		}
+	 }
+?>
+
+<!doctype html>
+<!doctype html>
+<html class="no-js" lang="zxx">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title>Avatar  | Inicio de Sesión</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Favicons -->
+	<link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="apple-touch-icon" href="images/icon.png">
+
+	<!-- Google font (font-family: 'Roboto', sans-serif; Poppins ; Satisfy) -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,500,600,600i,700,700i,800" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/plugins.css">
+	<link rel="stylesheet" href="style.css">
+
+	<!-- Cusom css -->
+   <link rel="stylesheet" href="css/custom.css">
+
+	<!-- Modernizer js -->
+	<script src="js/vendor/modernizr-3.5.0.min.js"></script>
+</head>
+<body>
+	<!--[if lte IE 9]>
+		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+	<![endif]-->
+
+	<!-- Main wrapper -->
+	<div class="wrapper" id="wrapper">
+		<!-- Header -->
+		<header id="wn__header" class="header__area header__absolute sticky__header">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-6 col-sm-6 col-6 col-lg-2">
+						<div class="logo">
+							<a href="index.html">
+								<img src="images/logo/Logo90.png" alt="logo Avatar">
+							</a>
+						</div>
+					</div>
+					<div class="col-lg-8 d-none d-lg-block">
+						<nav class="mainmenu__nav">
+							<ul class="meninmenu d-flex justify-content-start">
+								<li class="drop with--one--item"><a href="index.html">Início</a></li>
+								<li class="drop"><a href="#">Comprar</a>
+									<div class="megamenu mega03">
+										<ul class="item item03">
+											<li class="title">Comunicate</li>
+											<li><a href="faq.html">Preguntas Frecuentas</a></li>
+											<li><a href="contact.html">Contactanos</a></li>
+										</ul>
+										<ul class="item item03">
+											<li class="title">Tienda</li>
+											<li><a href="my-account.html">Mi cuenta</a></li>
+											<li><a href="cart.html">Carrito</a></li>
+											<li><a href="checkout.html">Pagos</a></li>
+											<li><a href="wishlist.html">Mis favoritos</a></li>
+											<li><a href="faq.html">Preguntas frecuentes</a></li>
+										</ul>
+										<ul class="item item03">
+											<li class="title">Outlet</li>
+											<li><a href="shop-grid.html">Smartwacht</a></li>
+											<li><a href="shop-grid.html">Accesorios</a></li>
+										</ul>
+									</div>
+								</li>
+								<li class="drop"><a href="shop-grid.html">Gift card</a>
+									<div class="megamenu mega03">
+										<ul class="item item03">
+											<li class="title">regalos</li>
+											<li><a href="shop-grid.html">Mujer</a></li>
+											<li><a href="shop-grid.html">Hombre </a></li>
+											<li><a href="shop-grid.html">Niños </a></li>
+																				</ul>
+
+									</div>
+
+
+
+								</nav>
+					</div>
+					<div class="col-md-6 col-sm-6 col-6 col-lg-2">
+						<ul class="header__sidebar__right d-flex justify-content-end align-items-center">
+							<li class="shop_search"><a class="search__active" href="#"></a></li>
+							<li class="wishlist"><a href="#"></a></li>
+							<li class="shopcart"><a class="cartbox_active" href="#"><span class="product_qun">3</span></a>
+								<!-- Start Shopping Cart -->
+								<div class="block-minicart minicart__active">
+									<div class="minicart-content-wrapper">
+										<div class="micart__close">
+											<span>Agregá al carrito</span>
+										</div>
+										<div class="items-total d-flex justify-content-between">
+											<span>0 items</span>
+											<span>COSTO TOTAL</span>
+										</div>
+										<div class="total_amount text-right">
+											<span>$0.00</span>
+										</div>
+										<div class="mini_action checkout">
+											<a class="checkout__btn" href="cart.html">Pagá acá</a>
+										</div>
+										<div class="single__items">
+											<div class="miniproduct">
+												<div class="item01 d-flex">
+													<div class="thumb">
+														<!-- Start Shopping Cart
+														<a href="product-details.html"><img src="images/product/sm-img/1.jpg" alt="product images"></a>
+													</div>
+													<div class="content">
+														<h6><a href="product-details.html">Voyage Yoga Bag</a></h6>
+														<span class="prize">$30.00</span>
+														<div class="product_prize d-flex justify-content-between">
+															<span class="qun">Qty: 01</span>
+															<ul class="d-flex justify-content-end">
+																<li><a href="#"><i class="zmdi zmdi-settings"></i></a></li>
+																<li><a href="#"><i class="zmdi zmdi-delete"></i></a></li>
+															</ul>
+														</div>
+													</div>
+												</div>
+												<div class="item01 d-flex mt--20">
+													<div class="thumb">
+														<a href="product-details.html"><img src="images/product/sm-img/3.jpg" alt="product images">"esto se borra"</a>
+													</div>
+													<div class="content">
+														<h6><a href="product-details.html">Impulse Duffle</a></h6>
+														<span class="prize">$40.00</span>
+														<div class="product_prize d-flex justify-content-between">
+															<span class="qun">Qty: 03</span>
+															<ul class="d-flex justify-content-end">
+																<li><a href="#"><i class="zmdi zmdi-settings"></i></a></li>
+																<li><a href="#"><i class="zmdi zmdi-delete"></i></a></li>
+															</ul>
+														</div>
+													</div>
+												</div>
+												<div class="item01 d-flex mt--20">
+													<div class="thumb">
+														<a href="product-details.html"><img src="images/product/sm-img/2.jpg" alt="product images"></a>
+													</div>
+													<div class="content">
+														<h6><a href="product-details.html">Compete Track Tote</a></h6>
+														<span class="prize">$40.00</span>
+														<div class="product_prize d-flex justify-content-between">
+															<span class="qun">Qty: 03</span>
+															<ul class="d-flex justify-content-end">
+																<li><a href="#"><i class="zmdi zmdi-settings"></i></a></li>
+																<li><a href="#"><i class="zmdi zmdi-delete"></i></a></li>
+															</ul>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+										<div class="mini_action cart">
+											<a class="cart__btn" href="cart.html">View and edit cart</a>
+										</div>
+									</div>
+								</div>
+								-->
+								<!-- End Shopping Cart -->
+							</li>
+							<li class="setting__bar__icon"><a class="setting__active" href="#"></a>
+								<div class="searchbar__content setting__block">
+									<div class="content-inner">
+										<div class="switcher-currency">
+											<strong class="label switcher-label">
+												<span>Moneda</span>
+											</strong>
+											<div class="switcher-options">
+												<div class="switcher-currency-trigger">
+													<span class="currency-trigger">AR$</span>
+													<ul class="switcher-dropdown">
+														<li>AR$</li>
+														<li>Dolar</li>
+													</ul>
+												</div>
+											</div>
+										</div>
+										<div class="switcher-currency">
+											<strong class="label switcher-label">
+												<span>Idioma</span>
+											</strong>
+											<div class="switcher-options">
+												<div class="switcher-currency-trigger">
+													<span class="currency-trigger">Elegir</span>
+													<ul class="switcher-dropdown">
+														<li>Español</li>
+														<li>Inglés</li>
+														<li>Portugues</li>
+																											</ul>
+												</div>
+											<!-- End Shopping Cart
+											</div>
+										</div>
+
+										<div class="switcher-currency">
+											<strong class="label switcher-label">
+												<span>Select Store</span>
+											</strong>
+											<div class="switcher-options">
+												<div class="switcher-currency-trigger">
+													<span class="currency-trigger">Fashion Store</span>
+													<ul class="switcher-dropdown">
+														<li>Furniture</li>
+														<li>Shoes</li>
+														<li>Speaker Store</li>
+														<li>Furniture</li>
+													</ul>
+												</div>
+											</div>
+										</div>
+										<div class="switcher-currency">
+											<strong class="label switcher-label">
+												<span>My Account</span>
+											</strong>
+											<div class="switcher-options">
+												<div class="switcher-currency-trigger">
+													<div class="setting__menu">
+														<span><a href="#">Compare Product</a></span>
+														<span><a href="#">My Account</a></span>
+														<span><a href="#">My Wishlist</a></span>
+														<span><a href="#">Sign In</a></span>
+														<span><a href="#">Create An Account</a></span>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+				-->
+				<!-- Start Mobile Menu
+				<div class="row d-none">
+					<div class="col-lg-12 d-none">
+						<nav class="mobilemenu__nav">
+							<ul class="meninmenu">
+								<li><a href="index.html">Home</a></li>
+								<li><a href="#">Pages</a>
+									<ul>
+										<li><a href="about.html">About Page</a></li>
+										<li><a href="portfolio.html">Portfolio</a>
+											<ul>
+												<li><a href="portfolio.html">Portfolio</a></li>
+												<li><a href="portfolio-details.html">Portfolio Details</a></li>
+											</ul>
+										</li>
+										<li><a href="my-account.html">My Account</a></li>
+										<li><a href="cart.html">Cart Page</a></li>
+										<li><a href="checkout.html">Checkout Page</a></li>
+										<li><a href="wishlist.html">Wishlist Page</a></li>
+										<li><a href="error404.html">404 Page</a></li>
+										<li><a href="faq.html">Faq Page</a></li>
+										<li><a href="team.html">Team Page</a></li>
+									</ul>
+								</li>
+								<li><a href="shop-grid.html">Shop</a>
+									<ul>
+										<li><a href="shop-grid.html">Shop Grid</a></li>
+										<li><a href="single-product.html">Single Product</a></li>
+									</ul>
+								</li>
+								<li><a href="blog.html">Blog</a>
+									<ul>
+										<li><a href="blog.html">Blog Page</a></li>
+										<li><a href="blog-details.html">Blog Details</a></li>
+									</ul>
+								</li>
+								<li><a href="contact.html">Contact</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+				-->
+				<!-- End Mobile Menu -->
+	            <div class="mobile-menu d-block d-lg-none">
+	            </div>
+	            <!-- Mobile Menu -->
+			</div>
+		</header>
+		<!-- //Header -->
+		<!-- Start Search Popup -->
+		<div class="brown--color box-search-content search_active block-bg close__top">
+			<form id="search_mini_form" class="minisearch" action="#">
+				<div class="field__search">
+					<input type="text" placeholder="Search entire store here...">
+					<div class="action">
+						<a href="#"><i class="zmdi zmdi-search"></i></a>
+					</div>
+				</div>
+			</form>
+			<div class="close__wrap">
+				<span>close</span>
+			</div>
+		</div>
+		<!-- End Search Popup -->
+
+        	<!-- Start Single Slide
+	        <div class="slide animation__style10 bg-image--7 fullscreen align__center--left">
+	            <div class="container">
+	            	<div class="row">
+	            		<div class="col-lg-12">
+	            			<div class="slider__content">
+		            			<div class="contentbox">
+		            				<h2>Buy <span>your </span></h2>
+		            				<h2>favourite <span>Book </span></h2>
+		            				<h2>from <span>Here </span></h2>
+				                   	<a class="shopbtn" href="#">shop now</a>
+		            			</div>
+	            			</div>
+	            		</div>
+	            	</div>
+	            </div>
+			</div>
+			-->
+			<!-- End Single Slide -->
+        </div>
+		<!-- Start Search Popup -->
+		<div class="box-search-content search_active block-bg close__top">
+			<form id="search_mini_form" class="minisearch" action="#">
+				<div class="field__search">
+					<input type="text" placeholder="Search entire store here...">
+					<div class="action">
+						<a href="#"><i class="zmdi zmdi-search"></i></a>
+					</div>
+				</div>
+			</form>
+			<div class="close__wrap">
+				<span>close</span>
+			</div>
+		</div>
+		<!-- End Search Popup -->
+        <!-- Start Bradcaump area -->
+        <div class="ht__bradcaump__area bg-image--6">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="bradcaump__inner text-center">
+                        	<h2 class="bradcaump-title">Inicio de Sesión</h2>
+                            <nav class="bradcaump-content">
+                              <a class="breadcrumb_item" href="index.html">Inicio</a>
+                              <span class="brd-separetor">/</span>
+                              <span class="breadcrumb_item active">Mi cuenta</span>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Bradcaump area -->
+		<!-- Start My Account Area -->
+		<section class="my_account_area pt--80 pb--55 bg--white">
+			<div class="container">
+				<div class="row">
+                <div class="col-lg-6 col-12">
+						<div class="my__account__wrapper">
+							<h3 class="account__title">Logueate</h3>
+							<form action="#" method="post">
+								<div class="account__form">
+									<div class="input__box">
+										<label for="email">Email <span>*</span></label>
+										<input type="email" class="form-control" id="email" name="email" value=" <?= persistirDato($arrayDeErrores, "email") ?> ">
+										<small class="text-danger"><?= isset($arrayDeErrores["email"]) ? $arrayDeErrores["email"] : "" ?></small>
+									</div>
+									<div class="input__box">
+										<label for="pass">Clave<span>*</span></label>
+										<input type="password" class="form-control" id="pass" name="pass">
+										<small class="text-danger"><?= isset($arrayDeErrores["pass"]) ? $arrayDeErrores["pass"] : "" ?></small>
+									</div>
+									<div class="form__btn">
+										<button>Login</button>
+										<label class="label-for-checkbox">
+											<input id="rememberme" class="input-checkbox" name="rememberme" value="forever" type="checkbox">
+											<span>Recordarme</span>
+										</label>
+									</div>
+									<a class="forget_pass" href="#">No recuerdas tu clave?</a>
+								</div>
+							</form>
+						</div>
+					</div>
+					<!--<div class="col-lg-6 col-12">
+						<div class="my__account__wrapper">
+							<h3 class="account__title">Register</h3>
+							<form action="#">
+								<div class="account__form">
+									<div class="input__box">
+										<label>Email address <span>*</span></label>
+										<input type="email">
+									</div>
+									<div class="input__box">
+										<label>Password<span>*</span></label>
+										<input type="password">
+									</div>
+									<div class="form__btn">
+										<button>Register</button>
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>-->
+				</div>
+			</div>
+		</section>
+		<!-- End My Account Area -->
+		<!-- Footer Area -->
+		<footer id="wn__footer" class="footer__area bg__cat--8 brown--color">
+			<div class="footer-static-top">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-12">
+							<div class="footer__widget footer__menu">
+								<div class="ft__logo">
+									<a href="index.html">
+										<img src="images/logo/Logo90.png" alt="logo">
+									</a>
+									<p>Por cualquier duda puede dirigirse a <a href="">Términos y condiciones de uso del Sitio</a></p>
+								</div>
+								<div class="footer__content">
+									<ul class="social__net social__net--2 d-flex justify-content-center">
+										<li><a href="#"><i class="bi bi-facebook"></i></a></li>
+										<li><a href="#"><i class="bi bi-google"></i></a></li>
+										<li><a href="#"><i class="bi bi-twitter"></i></a></li>
+										<li><a href="#"><i class="bi bi-linkedin"></i></a></li>
+										<li><a href="#"><i class="bi bi-youtube"></i></a></li>
+									</ul>
+									<ul class="mainmenu d-flex justify-content-center">
+										<li><a href="inicio sesion.html">Loguin</a></li>
+										<li><a href="registro.html">Registrate</a></li>
+										<li><a href="index.html">Más vendidos</a></li>
+										<li><a href="shop-grid.html">Todos los productos</a></li>
+										<li><a href="faq.html">Preguntas Frecuentes</a></li>
+										<li><a href="contact.html">Contactanos</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="copyright__wrapper">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-6 col-md-6 col-sm-12">
+							<div class="copyright">
+								<div class="copy__right__inner text-left">
+									<p>Copyright <i class="fa fa-copyright"></i> <a href="">Avatar Company.</a> Todos los derechos reservados</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-12">
+							<div class="payment text-right">
+								<img src="images/icons/payment.png" alt="" />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<!-- //Footer Area -->
+
+	</div>
+	<!-- //Main wrapper -->
+
+	<!-- JS Files -->
+	<script src="js/vendor/jquery-3.2.1.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/plugins.js"></script>
+	<script src="js/active.js"></script>
+
+</body>
+</html>
